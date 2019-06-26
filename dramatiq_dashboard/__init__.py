a3db8050e42e0c124e8a8363a28672f54323497d
Bogdan Popa <bogdan@defn.io>
2019-06-26 09:27:28 +0300

    core: expose DashboardApp at the package level

--- dramatiq_dashboard/__init__.py
+++ dramatiq_dashboard/__init__.py
@@ -1,4 +1,5 @@
+from .application import DashboardApp
 from .middleware import make_wsgi_middleware
 
-__all__ = ["make_wsgi_middleware"]
+__all__ = ["DashboardApp", "make_wsgi_middleware"]
 __version__ = "0.2.0"
