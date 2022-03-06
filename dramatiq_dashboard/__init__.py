eba9daa721af4643a0206094569f219ef7e54665
Bogdan Popa <bogdan@defn.io>
2022-03-06 11:23:26 +0200

    build: bump version

--- dramatiq_dashboard/__init__.py
+++ dramatiq_dashboard/__init__.py
@@ -2,4 +2,4 @@ from .application import DashboardApp
 from .middleware import make_wsgi_middleware
 
 __all__ = ["DashboardApp", "make_wsgi_middleware"]
-__version__ = "0.2.3"
+__version__ = "0.3.0"
