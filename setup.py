66b92b7a413243bd2bf43d1521c56deae081bc15
Chris Sprehe <chris.sprehe@gmail.com>
2022-03-15 15:43:49 -0500

    Succint

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(rel("dramatiq_dashboard", "__init__.py"), "r") as f:
 dependencies = [
     "dataclasses; python_version < '3.7'",
     "dramatiq[redis]>=1.6,<2.0",
-    "jinja2>=2,>=3",
+    "jinja2>=2",
     "redis>=2.0,<5.0",
 ]
 
