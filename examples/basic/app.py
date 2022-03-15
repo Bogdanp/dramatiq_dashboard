9e08d388da6931fb5242cbaf7877573ca2d62d93
Chris Sprehe <chris.sprehe@gmail.com>
2022-03-15 15:34:59 -0500

    Update basic example and dependency reqs to support Jinja2 version 3.x

--- examples/basic/app.py
+++ examples/basic/app.py
@@ -37,11 +37,7 @@ def start(uri: QueryParam):
     return "Message sent!"
 
 
-app = App(
-    routes=[
-        Route("/crawl", start)
-    ]
-)
+app = App(routes=[Route("/crawl", start)])
 
 dashboard_middleware = dramatiq_dashboard.make_wsgi_middleware("/drama")
 app = dashboard_middleware(app)
